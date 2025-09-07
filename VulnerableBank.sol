a3c647e7405693da39a31c56635639bb835675b7
zvx1s <zaki.sh41k@gmail.com>
2025-09-06 23:29:47 -0700

    Update VulnerableBank.sol

--- VulnerableBank.sol
+++ VulnerableBank.sol
@@ -2,17 +2,16 @@
 pragma solidity ^0.8.20;
 
 contract VulnerableBank {
-    mapping(address => uint256) public balances;
+    mapping(address => uint) public balances;
 
     function deposit() external payable {
         balances[msg.sender] += msg.value;
     }
 
-    // BUG: interaction before effects (reentrancy)
-    function withdraw(uint256 amount) external {
-        require(balances[msg.sender] >= amount, "insufficient");
+    function withdraw(uint amount) external {
+        require(balances[msg.sender] >= amount);
         (bool ok,) = msg.sender.call{value: amount}("");
-        require(ok, "send failed");
+        require(ok);
         balances[msg.sender] -= amount;
     }
 }
