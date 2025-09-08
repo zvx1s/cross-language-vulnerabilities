5f8eadc77758a65e6a5577771be0be47fd06d8f9
zvx1s <zaki.sh41k@gmail.com>
2025-09-07 19:01:29 -0700

    Update FixedBank.rs

--- reentrancy-basic/rust/FixedBank.rs
+++ reentrancy-basic/rust/FixedBank.rs
@@ -8,8 +8,7 @@ impl FixedBank {
     pub fn deposit(&mut self, who: &str, amount: u64) {
         *self.balances.entry(who.into()).or_default() += amount;
     }
-
-    // Fixed: effects before interaction
+    
     pub fn withdraw(&mut self, who: &str, amount: u64, attacker: &mut dyn FnMut()) {
         assert!(self.balances[who] >= amount);
         self.balances.insert(who.into(), self.balances[who] - amount);
